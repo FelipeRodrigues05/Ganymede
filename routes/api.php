237d2f94bc6e1ea39ff389f30abd29f43f369906
FelipeRodrigues05 <felipebr998@gmail.com>
2023-03-10 16:18:07 -0300

    Routes of the Application using Prefix Routing

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,18 @@
 <?php
 
+use App\Http\Controllers\Api\ClientController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::prefix('users')->group(function () {
+    Route::get('/list', [ClientController::class, 'listAllClients']);
+    Route::post('/new', [ClientController::class, 'createClient']);
+    Route::delete('/del/{id}', [ClientController::class, 'deleteClient']);
+    Route::put('/upt/{id}', [ClientController::class, 'updateClient']);
+});
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get("/", function () {
+    return response()->json([
+        'success' => true
+    ]);
 });
