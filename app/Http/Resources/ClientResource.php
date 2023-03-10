c5a662b163e4dced4f1a49eea533f5c7a4c07de3
FelipeRodrigues05 <felipebr998@gmail.com>
2023-03-10 17:10:57 -0300

    Response Formatting

--- app/Http/Resources/ClientResource.php
+++ app/Http/Resources/ClientResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,6 +15,13 @@ class ClientResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'identify' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'phone' => $this->phone,
+            'address' => $this->address,
+            'created_at' => Carbon::make($this->created_at)->format('Y-m-d H:i')
+        ];
     }
 }
