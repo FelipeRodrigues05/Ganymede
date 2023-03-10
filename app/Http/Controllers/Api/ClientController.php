f90390fa52f3a8ae0dcaa8960be7fb00ee3f3891
FelipeRodrigues05 <felipebr998@gmail.com>
2023-03-10 17:10:22 -0300

    All functions implemented

--- app/Http/Controllers/Api/ClientController.php
+++ app/Http/Controllers/Api/ClientController.php
@@ -3,24 +3,43 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RequestClientRules;
+use App\Http\Resources\ClientResource;
 use App\Models\Client;
-use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
-    public function listAllClients() {
-        return Client::all();
+    public function listAllClients()
+    {
+        $clients = Client::all();
+
+        // RESOURCES FACILITAM A LEITURA
+        return ClientResource::collection($clients);
     }
 
-    public function createClient(Request $request) {
+    // REQUESTCLIENTRULES faz parte da validação dos dados
+    public function createClient(RequestClientRules $request)
+    {
+        $data = $request->validated();
+        $client = Client::create($data);
 
+        return new ClientResource($client);
     }
 
-    public function deleteClient(Request $request) {
+    public function deleteClient(string $id)
+    {
+        Client::findOrFail($id)->delete();
 
+        return response()->json([], 200);
     }
 
-    public function updateClient(Request $request) {
+    public function updateClient(string $id, RequestClientRules $request)
+    {
+        $data = $request->validated();
+        $client = Client::findOrFail($id);
 
+        $client->update($data);
+
+        return new ClientResource($client);
     }
 }
