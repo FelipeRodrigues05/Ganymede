f127e36f146d6afeefd1833032274ad8c2c351cf
FelipeRodrigues05 <felipebr998@gmail.com>
2023-04-03 14:52:56 -0300

    refactor: more simple to understand

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,23 @@
 <?php
 
 use App\Http\Controllers\Api\ClientController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('users')->group(function () {
-    Route::get('/list', [ClientController::class, 'listAllClients']);
-    Route::post('/new', [ClientController::class, 'createClient']);
-    Route::delete('/del/{id}', [ClientController::class, 'deleteClient']);
-    Route::put('/upt/{id}', [ClientController::class, 'updateClient']);
+Route::controller(ClientController::class)->group(function () {
+    Route::prefix('clients')->group(function () {
+        Route::get('/list',  'listAllClients')->name('clients.list');
+        Route::get('/list/{id}',  'listOneClient')->name('clients.listOne');
+        Route::post('/new', 'createClient')->name('clients.new');
+        Route::delete('/del/{id}', 'deleteClient')->name('clients.delete');
+        Route::put('/upt/{id}', 'updateClient')->name('clients.update');
+    });
 });
+
+// Route::controller(InvoiceController::class)->group(function () {
+//     Route::prefix('invoices')->group(function () {
+//         // ROTAS INVOICE
+//     });
+// });
 
 Route::get("/", function () {
     return response()->json([
